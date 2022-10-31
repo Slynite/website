24a7d15020580455aac5c66f737acfdd065c80e9
Danny Schapeit <dannyschapeit@slynite.com>
2022-10-31 17:02:53 +0100

    Improve Images in MD files
    * create custom component to use next/image instead of <img>
    * add modal using headlessui for bigger images

--- components/utils/markdown.js
+++ components/utils/markdown.js
@@ -2,10 +2,32 @@ import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import rehypeRaw from 'rehype-raw';
 import styles from '../../styles/markdown-styles.module.css';
+import ImageWithModal from './imageWithModal';
 
 export default function Markdown({ content }) {
-  return (
+  const customComponents = {
+    img: ({ src, alt, width, height }) => {
+      if (width == null) {
+        width = 350;
+      }
+
+      if (height == null) {
+        height = 200;
+      }
+
+      return (
+        <ImageWithModal
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+        />
+      );
+    }
+  }
+  
+  return ( 
       // eslint-disable-next-line react/no-children-prop
-      <ReactMarkdown className={styles.markdown} rehypePlugins={[rehypeRaw, rehypeHighlight]} children={content} />
+      <ReactMarkdown className={styles.markdown} rehypePlugins={[rehypeRaw, rehypeHighlight]} components={customComponents} children={content} />
   );
 }
