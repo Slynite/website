74f2e6da4efe641c578dca9bb0359e84dfcfc99e
Danny Schapeit <dannyschapeit@slynite.com>
2023-08-12 19:46:55 +0200

    Fix use of dictionary
    - remove "use server" from file

--- src/app/[lang]/dictionaries.tsx
+++ src/app/[lang]/dictionaries.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 const dictionaries: any = {
   en: () => import('../../../dictionaries/en.json').then((module) => module.default),
   de: () => import('../../../dictionaries/de.json').then((module) => module.default),
