9338cefa9741ee5e013396bfe2e600cffe88cab9
Danny Schapeit <dannyschapeit@slynite.com>
2023-09-03 12:07:24 +0200

    WEB-24 404 Page
    - add not-found.tsx ad root folder to handle 404 errors
    - move layout.tsx to root folder to cover all pages
    - add i18nHelper.ts to get current language in server components
    - move available langauges to i18nHelper.availableLanguages()
    - add translation strings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { availableLanguages } from '../lib/i18nHelper'
 
-let locales = ['en', 'de']
+let locales = availableLanguages();
 
 export function middleware(request: NextRequest) {
     let locale = "en"
@@ -24,4 +25,4 @@ export const config = {
     matcher: [
       '/((?!api|static|.*\\..*|_next).*)',
     ],
-  }
+}
