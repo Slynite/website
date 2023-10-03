83530c036c2ae3ec36639cb5910ab364e1996846
Danny Schapeit <dannyschapeit@slynite.com>
2023-10-03 15:01:27 +0200

    Improve language switcher
    - refresh page to reload components which are not in dynamic route

--- src/components/language/switcher.tsx
+++ src/components/language/switcher.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function LanguageSwitcher() {
     const pathname = usePathname();
     const langKey = pathname.split("/")[1];
 
+    const router = useRouter();
+    function handleLangSwitch(lang: string) {
+        router.push(pathname.replace(langKey, lang));
+        router.refresh();
+    }
+
     if (langKey === "de") {
         return(
             <div className="flex space-x-1 justify-end">
-                <Link href={pathname.replace(langKey, "en")} as={pathname.replace(langKey, "en")}>English</Link>
+                <button className="text-zinc-200" onClick={() => handleLangSwitch("en")}>English</button>
                 <p>/</p>
                 <p className="underline">Deutsch</p>
             </div>
@@ -19,7 +25,7 @@ export default function LanguageSwitcher() {
             <div className="flex space-x-1 justify-end">
                 <p className="underline">English</p>
                 <p>/</p>
-                <Link href={pathname.replace(langKey, "de")} as={pathname.replace(langKey, "de")}>Deutsch</Link>
+                <button className="text-zinc-200" onClick={() => handleLangSwitch("de")}>Deutsch</button>
             </div>
         )
     }
