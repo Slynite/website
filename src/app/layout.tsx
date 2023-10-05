b99439d983181bf49db7a3972f3ec97f416ffbce
Danny Schapeit <dannyschapeit@slynite.com>
2023-10-05 07:18:50 +0200

    WEB-27 Fix issues before launch
    - fix i18n routing by add child layout and create dynamic not-found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
-import Footer from "@/components/footer";
 import "./[lang]/globals.css";
 import { Roboto } from "next/font/google";
-import Header from "@/components/header";
-import { getMainDictionary } from "./[lang]/dictionaries";
-import { cookies } from "next/headers";
-import { getLangFromCookie } from "../../lib/i18nHelper";
 import { Metadata } from "next";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
@@ -43,17 +38,10 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
-	const cookieList = cookies();
-	const dict = await getMainDictionary(getLangFromCookie(cookieList.get("lang")));
-
     return (
-		<html lang={dict.lang}>
+		<html lang="en">
 			<body className={`${roboto.className} text-gray-200 flex flex-col min-h-screen h-screen container mx-auto text-sm px-6 lg:px-5 xl:px-36 2xl:px-64`}>
-				<Header dict={dict}/>
-				<div className="flex-grow">
-                    {children}
-                </div>
-                <Footer dict={dict}/>
+				{children}
 			</body>
 		</html>
 	);
