8dc87c418bdbb67bba5fc705dd53c5eb40ae20d0
Danny Schapeit <dannyschapeit@slynite.com>
2024-10-01 05:14:24 +0000

    improve smtp logging and error catch

--- src/app/api/form/route.ts
+++ src/app/api/form/route.ts
@@ -8,48 +8,64 @@ const mailer = require("nodemailer");
 const template_path = path.join(__dirname, '../../../../../public/mail-templates/');
  
 export async function POST(request: NextRequest) {
-    let formData: any = await request.json();
+    try {
+        let formData: any = await request.json();
     
-    if (!formData || !formData.email || !formData.subject || !formData.message) {
-        return NextResponse.json({ status: "error", message: "formdata missing" })
-    }
-
-    const clientMail = await rewriteMailContent({ file: template_path + "contact-form-user", email: formData.email, subject: formData.subject, message: formData.message });
-    const teamMail = await rewriteMailContent({ file: template_path + "contact-form-team", email: formData.email, subject: formData.subject, message: formData.message });
-
-    //create reuseable transporter for SMTP transporter
-    let transporter = mailer.createTransport({
-        host: process.env.MAIL_SMTP_HOST,
-        port: process.env.MAIL_SMTP_PORT,
-        secure: true, // true for 465, false for other ports
-        auth: {
-            user: process.env.MAIL_ADRESS,
-            pass: process.env.MAIL_PASSWORD,
-        },
-    });
-
-    let sendMailToSlynite = await transporter.sendMail({
-        from: process.env.MAIL_NAME + '<' + process.env.MAIL_ADRESS +'>',
-        to: "hello@slynite.com",
-        subject: "A new message from Slynite.com contact form",
-        html: teamMail
-    });
-
-    if (sendMailToSlynite.messageId != null && sendMailToSlynite.messageId != undefined) {
-        try {
-            await transporter.sendMail({
-                from: process.env.MAIL_NAME + '<' + process.env.MAIL_ADRESS +'>',
-                to: formData.email,
-                subject: "We received your message",
-                html: clientMail
-            });
-        } catch(e) {
-            return NextResponse.json({ status: "warning", message: "mail to " + formData.email + " failed." })
+        if (!formData || !formData.email || !formData.subject || !formData.message) {
+            return NextResponse.json({ status: "error", message: "formdata missing" })
         }
-        return NextResponse.json({ status: "success", message: "complete" })
-    } else {
+
+        let smtpHost = process.env.MAIL_SMTP_HOST
+        let smtpPort = process.env.MAIL_SMTP_PORT
+        let smtpUser = process.env.MAIL_ADRESS
+        let smtpPassword =  process.env.MAIL_PASSWORD
+
+        if (smtpHost == undefined || smtpPort == undefined || smtpUser == undefined || smtpPassword == undefined) {
+            throw Error("SMTP configuration not properly set. host=" + smtpHost + " port=" + smtpPort + " user=" + smtpUser + " password=" + smtpPassword)
+        }
+    
+        const clientMail = await rewriteMailContent({ file: template_path + "contact-form-user", email: formData.email, subject: formData.subject, message: formData.message });
+        const teamMail = await rewriteMailContent({ file: template_path + "contact-form-team", email: formData.email, subject: formData.subject, message: formData.message });
+
+        //create reuseable transporter for SMTP transporter
+        let transporter = mailer.createTransport({
+            host: process.env.MAIL_SMTP_HOST,
+            port: process.env.MAIL_SMTP_PORT,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user: process.env.MAIL_ADRESS,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        });
+    
+        let sendMailToSlynite = await transporter.sendMail({
+            from: process.env.MAIL_NAME + '<' + process.env.MAIL_ADRESS +'>',
+            to: "hello@slynite.com",
+            subject: "A new message from Slynite.com contact form",
+            html: teamMail
+        });
+    
+        if (sendMailToSlynite.messageId != null && sendMailToSlynite.messageId != undefined) {
+            try {
+                await transporter.sendMail({
+                    from: process.env.MAIL_NAME + '<' + process.env.MAIL_ADRESS +'>',
+                    to: formData.email,
+                    subject: "We received your message",
+                    html: clientMail
+                });
+            } catch(e) {
+                return NextResponse.json({ status: "warning", message: "mail to " + formData.email + " failed." })
+            }
+            return NextResponse.json({ status: "success", message: "complete" })
+        } else {
+            return NextResponse.json({ status: "error", message: "error while sending mail" })
+        }
+    } catch (e) {
+
+        console.error("Something went wrong by sending an email. Error " + e)
         return NextResponse.json({ status: "error", message: "error while sending mail" })
     }
+    
 }
 
 async function rewriteMailContent({ file, email, subject, message }: { file: string, email: string, subject: string, message: string }): Promise<any> {
